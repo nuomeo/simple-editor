4fae6ffe2775982fe8e7f831bbe471cd944ed71d
gaoyuan <gaoyuan@secretlisa.com>
2025-05-14 18:03:54 +0800

    feat: 新增判空以及加载数据方法

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,11 +1,6 @@
+#[derive(Default)]
 pub struct Buffer {
     data: Vec<String>,
-}
-
-impl Default for Buffer {
-    fn default() -> Self {
-        Buffer { data: vec!["hello world".to_string(), "this is a test buffer".to_string()] }
-    }
 }
 
 impl Buffer {
@@ -20,5 +15,18 @@ impl Buffer {
             }
             _ => "hello, world".to_string() // TODO: handle this better
         }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+
+    pub fn load(filename: &str) -> Result<Self, std::io::Error> {
+        let contents = std::fs::read_to_string(filename)?;
+        let mut data = Vec::new();
+        for line in contents.lines() {
+            data.push(String::from(line));
+        }
+        Ok(Self { data })
     }
 }
